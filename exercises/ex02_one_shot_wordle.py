f102df7c137e9bd9ffd165dc5209eac56d2629ca
Sunthoshini Premsankar <spremsankar@unc.edu>
2022-01-31 18:54:35 -0500

    Progress on Exercise 1

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -45,3 +45,4 @@ if word == secret_word:
 # this part is gauged by the actual word instead of the guess_emoji because the guess_emoji can change depending on the number of letters in the secret word
 else:
     print("Not quite. Play again soon!")
+    
